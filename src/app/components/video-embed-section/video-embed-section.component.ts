9ae963672d954cbb6ea544ce7c3f441b1ea271b6
razorsyntax <biomancefilms@hotmail.com>
2024-08-08 13:27:13 -0500

    Removed YT Video and Replaced with Odysee

--- src/app/components/video-embed-section/video-embed-section.component.ts
+++ src/app/components/video-embed-section/video-embed-section.component.ts
@@ -1,14 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-video-embed-section',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule],
   templateUrl: './video-embed-section.component.html',
   styleUrl: './video-embed-section.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class VideoEmbedSectionComponent { }
+export class VideoEmbedSectionComponent {
+  videoSource: SafeResourceUrl | string = '';
+  readonly url =
+    'https://odysee.com/$/embed/@whatcanyousee:0/actual-results:b?r=Cq86SrLjYfGeYEz6jRHHe4NppZ5FBQu2';
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  ngOnInit(): void {
+    this.videoSource = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
+  }
+}
